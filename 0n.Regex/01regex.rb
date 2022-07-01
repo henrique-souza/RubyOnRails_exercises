79d1a4eae9c016071a1c72d82772c4d1b709a316
Henrique Souza <hs.santos@totvs.com.br>
2022-07-01 00:14:58 -0300

    regex ruby

---
+++ 0n.Regex/01regex.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Simbora aprender a usar Regex com Ruby
+
+# 'Do you like dogs?' =~ /like/
+
+if 'Do you like dogs?'.match(/like/)
+  puts 'Passed'
+else
+  puts 'Not passed'
+end
+
