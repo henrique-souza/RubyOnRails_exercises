203ded06ca0c190837f94e3499d8e9019b6281f3
Henrique Souza <hs.santos@totvs.com.br>
2022-07-02 20:39:24 -0300

    regex ruby

--- 0n.Regex/01regex.rb
+++ 0n.Regex/01regex.rb
@@ -10,3 +10,20 @@ else
   puts 'Not passed'
 end
 
+puts("\n")
+
+def contem_vogal(string)
+  string =~ /[aeiou]/
+end
+
+puts contem_vogal('testando') #=> 1
+
+puts("\n")
+
+def contem_numero(string)
+  string =~ /\d/
+  # /\d/ expressão regular para números de 0 a 9 ou [0-9]
+end
+
+puts contem_numero('Ano 2022') #=> 4
+puts contem_numero('O Bruce é preto e branco') #=> nil
