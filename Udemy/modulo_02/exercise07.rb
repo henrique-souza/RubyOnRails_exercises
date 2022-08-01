90addc1e20903298129ca15c5ce7a25beee2a5ad
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-08-01 20:52:52 -0300

    update course

--- Udemy/modulo_02/exercise07.rb
+++ Udemy/modulo_02/exercise07.rb
@@ -1,4 +1,11 @@
-# 22
-# class Pessoa
-#
-# end
+# frozen_string_literal: true
+
+class Pessoa
+  def falar
+    'Olá, pessoal!'
+  end
+end
+
+pessoa = Pessoa.new
+
+puts pessoa.falar # => Olá, pessoal!
