5759a9e531cf79159e9da4dce302d1da59e6fb7c
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-08-04 18:52:28 -0300

    update course

--- Udemy/modulo_02/exercise08.rb
+++ Udemy/modulo_02/exercise08.rb
@@ -1,1 +1,23 @@
+# frozen_string_literal: true
 
+# classe Pessoa
+class Pessoa
+
+  def falar
+    'Olá, tudo bem?'
+  end
+
+  def meu_id
+    "Meu ID é o #{object_id}" # " #{self.object_id} " is redundant
+  end
+end
+
+primeira_pessoa = Pessoa.new
+
+puts primeira_pessoa.falar
+puts primeira_pessoa.meu_id # => 60
+
+segunda_pessoa = Pessoa.new
+
+puts segunda_pessoa.falar
+puts segunda_pessoa.meu_id # => 80
---
+++ Udemy/modulo_02/exercise09.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# reabrindo a classe String
+class String
+  def inverter_string
+    reverse
+  end
+end
+
+puts 'Henrique'.inverter_string
