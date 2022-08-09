8ffe122b29b504e6b7dfca086db8bc9c6ae0cdf1
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-08-09 07:19:49 -0300

    update course

---
+++ Course/exercises_01/modules/constants/app.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'payment'
+
+# include Payment
+
+puts Payment::PI_NUMBER # => 3.14
+# puts PI_NUMBER # => 3.14
---
+++ Course/exercises_01/modules/constants/payment.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# modules and mixins
+
+module Payment
+  PI_NUMBER = 3.14
+end
