6446da9555eed88b5198c4594eef8deae7c50f71
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-09-28 00:08:51 -0300

    remove Diamonds folder

--- TDD/Diamonds/lib/contain_diamonds.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-# Fazer um algoritmo que lê qualquer expressão
-# a filtra e extrai 'diamantes' dela, no formato "<>".
--- TDD/Diamonds/spec/contain_diamonds_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'contain_diamonds'
-# Ler comentário na contain_diamonds.rb
-RSpec.describe Diamonds do
-
-end
