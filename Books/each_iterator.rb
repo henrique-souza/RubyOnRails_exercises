a31f7e50cdc64ecd794386cbaa4a1dc6b5fb8d4e
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-09-28 01:37:53 -0300

    update books exercises

--- Books/each_iterator.rb
+++ Books/each_iterator.rb
@@ -7,8 +7,8 @@ array[3] = array[2] - 1
 array.each { |elements| print elements + 1 } # [4, 3, 2, 1]
 
 a = [1, 2, 3, 4]
-b = a.map { |value| print value * value } # [1, 4, 9, 16]
-c = a.select { |value| (value % 2).zero? }  # seleciona apenas pares [2, 4]
+_b = a.map { |value| print value * value } # [1, 4, 9, 16]
+_c = a.select { |value| (value % 2).zero? } # seleciona apenas pares [2, 4]
 
 a.inject do |sum, value|
   sum + value
