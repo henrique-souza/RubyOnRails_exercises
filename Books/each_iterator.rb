a3c903d5c1aa263c15f00e52a7ce5a9b983bdac6
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-09-28 01:36:57 -0300

    update books exercises

---
+++ Books/each_iterator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+array = [3, 2, 1]
+
+array[3] = array[2] - 1
+
+array.each { |elements| print elements + 1 } # [4, 3, 2, 1]
+
+a = [1, 2, 3, 4]
+b = a.map { |value| print value * value } # [1, 4, 9, 16]
+c = a.select { |value| (value % 2).zero? }  # seleciona apenas pares [2, 4]
+
+a.inject do |sum, value|
+  sum + value
+end
+
+# pagina 17 antes de hashes
+
