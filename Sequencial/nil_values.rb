5ee0c91a5db38cae4de0f5436aeeb8ea8df9e6c6
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-12-19 23:42:36 -0300

    update ruby exercises

--- Sequencial/nil_values.rb
+++ Sequencial/nil_values.rb
@@ -1,8 +1,30 @@
 # frozen_string_literal: true
 
-# name = 'Henrique' #=> Imprime a mensagem
-name = nil #=> Imprime nada
+name = 'Henrique' #=> Imprime a mensagem
+# name = nil #=> Imprime nada
 
 # puts "Welcome #{name}" if not name.nil?
 
-puts "Welcome, #{name}!" unless name.nil?
+# puts "Welcome, #{name}!" unless name.nil?
+
+puts "Welcome, #{name}!" if name
+
+puts
+
+# for number in (1..100) do
+#   puts "Número: #{number}"
+# end
+
+# uma forma melhor de fazer  mesmo que o de cima
+(1..100).each do |number|
+  puts "Número: #{number}"
+end
+
+puts
+
+while_number = 0
+
+while while_number <= 100
+  puts "Número: #{while_number}"
+  while_number += 1
+end
