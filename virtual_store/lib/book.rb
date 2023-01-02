45347d78d6aaa99b532e0abac2942609d9ec4007
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-01-02 01:22:21 -0300

    update ruby exercises

--- virtual_store/lib/book.rb
+++ virtual_store/lib/book.rb
@@ -13,6 +13,18 @@
 # Classe Book
 class Book
   def initialize(author, isbn = '1', pages_numbers)
-    puts "Author: #{author}, ISBN: #{isbn}, Pages: #{pages_numbers}"
+    # variáveis com @ são variáveis de instância
+    # as mesmas já são criadas encapsuladas como privadas
+    @author = author
+    @isbn = isbn
+    @pages_numbers = pages_numbers
+    # puts "Author: #{author}, ISBN: #{isbn}, Pages: #{pages_numbers}"
   end
 end
+
+ruby_and_rails_modern = Book.new('Henrique Souza', '229292', 213)
+rails_7_best = Book.new('Henrique Santos', '21038', 946)
+
+# puts ruby_and_rails_modern #=> #<Book:0x00007fa22aa778c0>
+p ruby_and_rails_modern #=> #<Book:0x00007fa22aa778c0 @author="Henrique Souza", @isbn="229292", @pages_numbers=213>
+p rails_7_best #=> #<Book:0x00007fa22aa777d0 @author="Henrique Santos", @isbn="21038", @pages_numbers=946>
